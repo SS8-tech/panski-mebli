daadfdd26f3a02f8933cadfefa27e313f054bf06
SS8-tech <stasonzhupanskyi@gmail.com>
2026-07-15 00:58:40 +0300

    port 465

--- server.js
+++ server.js
@@ -14,14 +14,19 @@ const PORT = process.env.PORT || 3000;
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    dns: {
-        family: 4
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log("SMTP ERROR:", error);
+    } else {
+        console.log("SMTP READY");
     }
 });
 
